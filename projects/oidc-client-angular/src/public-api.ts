1010f3c7081d8c1926319c6993b8d690b1f884ae
Yakov Kiffel <ykiffel@crossriverbank.com>
2020-10-26 17:08:27 -0400

    Export User, AuthComponent, and SilentRenewComponent via public api

--- projects/oidc-client-angular/src/public-api.ts
+++ projects/oidc-client-angular/src/public-api.ts
@@ -2,19 +2,9 @@
  * Public API Surface of oidc-client-angular
  */
 
-// // export * from './lib/components/auth/auth.component';
-// // export * from './lib/components/silent-renew/silent-renew.component';
-// export * from './lib/enums/log-level.enum';
-// export * from './lib/models/auth-settings.model';
-// // export * from './lib/oidc-client-angular.component';
-// export * from './lib/oidc-client-angular.module';
-// export * from './lib/oidc-client-angular.service';
-// export * from './lib/services/auth-interceptor.service';
-// export * from './lib/services/auth-settings.service';
-// export * from './lib/services/auth.service';
-// export * from './lib/services/logger.service';
-// export * from './lib/services/noop-logger.service';
-
+export { User } from 'oidc-client';
+export { AuthComponent } from './lib/components/auth/auth.component';
+export { SilentRenewComponent } from './lib/components/silent-renew/silent-renew.component';
 export { LogLevel } from './lib/enums/log-level.enum';
 export { AuthSettings } from './lib/models/auth-settings.model';
 export { OidcClientAngularModule } from './lib/oidc-client-angular.module';
@@ -23,6 +13,4 @@ export { AuthSettingsService } from './lib/services/auth-settings.service';
 export { AuthService } from './lib/services/auth.service';
 export { LoggerService } from './lib/services/logger.service';
 export { NoopLoggerService } from './lib/services/noop-logger.service';
-
-
 
